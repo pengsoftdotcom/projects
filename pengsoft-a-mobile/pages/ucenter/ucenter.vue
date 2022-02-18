99a2fd8cc745f32b326eabe05d9e7d46ae329e82
worry <12694525@qq.com>
2022-02-19 00:22:59 +0800

    init

---
+++ pengsoft-a-mobile/pages/ucenter/ucenter.vue
@@ -0,0 +1,175 @@
+<template>
+	<view class="center">
+		<view class="userInfo" @click.capture="toUserInfo">
+			<image class="logo-img" src="@/static/uni-center/defaultAvatarUrl.png"></image>
+			<view class="logo-title">
+				<text class="uer-name">用户名</text>
+			</view>
+		</view>
+		<uni-list class="center-list" v-for="(sublist , index) in ucenterList" :key="index">
+			<uni-list-item v-for="(item,i) in sublist" :title="item.title" link :rightText="item.rightText" :key="i"
+				:clickable="true" :to="item.to" @click="ucenterListClick(item)" :show-extra-icon="true"
+				:extraIcon="{type:item.icon,color:'#999'}">
+				<template v-slot:footer>
+					<view v-if="item.showBadge" class="item-footer">
+						<text class="item-footer-text">{{item.rightText}}</text>
+						<view class="item-footer-badge"></view>
+					</view>
+				</template>
+			</uni-list-item>
+		</uni-list>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				ucenterList: [
+					[{
+							"title": '我的项目',
+							"event": 'signInByAd',
+							"icon": "compose"
+						},
+						{
+							"title": '劳动合同',
+							"event": 'signIn',
+							"icon": "compose"
+						},
+						{
+							"title": '安全教育',
+							"event": 'gotoMarket',
+							"icon": "hand-thumbsup"
+						},
+					],
+					[{
+						"title": '修改密码',
+						"to": '/uni_modules/uni-feedback/pages/opendb-feedback/opendb-feedback',
+						"icon": "help"
+					}, ],
+					[{
+						"title": '关于',
+						"to": '/pages/ucenter/about/about',
+						"icon": "info"
+					}]
+				],
+			}
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+	/* #ifndef APP-NVUE */
+	view {
+		display: flex;
+		box-sizing: border-box;
+		flex-direction: column;
+	}
+
+	page {
+		background-color: #f8f8f8;
+	}
+
+	/* #endif*/
+
+	.center {
+		flex: 1;
+		flex-direction: column;
+		background-color: #f8f8f8;
+	}
+
+	.userInfo {
+		width: 750rpx;
+		padding: 20rpx;
+		padding-top: 50px;
+		background-image: url(../../static/uni-center/headers.png);
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.logo-img {
+		width: 150rpx;
+		height: 150rpx;
+		border-radius: 150rpx;
+	}
+
+	.logo-title {
+		flex: 1;
+		align-items: center;
+		justify-content: space-between;
+		flex-direction: row;
+	}
+
+	.uer-name {
+		height: 100rpx;
+		line-height: 100rpx;
+		font-size: 38rpx;
+		color: #FFFFFF;
+	}
+
+	.center-list {
+		margin-bottom: 30rpx;
+		background-color: #f9f9f9;
+	}
+
+	.center-list-cell {
+		width: 750rpx;
+		background-color: #007AFF;
+		height: 40rpx;
+	}
+
+	.grid {
+		background-color: #FFFFFF;
+		margin-bottom: 6px;
+	}
+
+	.uni-grid .text {
+		font-size: 30rpx;
+		height: 25px;
+		line-height: 25px;
+		color: #817f82;
+	}
+
+	.uni-grid .item ::v-deep .uni-grid-item__box {
+		justify-content: center;
+		align-items: center;
+	}
+
+
+	/*修改边线粗细示例*/
+	/* #ifndef APP-NVUE */
+	.center-list ::v-deep .uni-list--border:after {
+		-webkit-transform: scaleY(0.2);
+		transform: scaleY(0.2);
+		margin-left: 80rpx;
+	}
+
+	.center-list ::v-deep .uni-list--border-top,
+	.center-list ::v-deep .uni-list--border-bottom {
+		display: none;
+	}
+
+	/* #endif */
+	.item-footer {
+		flex-direction: row;
+		align-items: center;
+	}
+
+	.item-footer-text {
+		color: #999;
+		font-size: 24rpx;
+		padding-right: 10rpx;
+	}
+
+	.item-footer-badge {
+		width: 20rpx;
+		height: 20rpx;
+		/* #ifndef APP-NVUE */
+		border-radius: 50%;
+		/* #endif */
+		/* #ifdef APP-NVUE */
+		border-radius: 10rpx;
+		/* #endif */
+		background-color: #DD524D;
+	}
+</style>
